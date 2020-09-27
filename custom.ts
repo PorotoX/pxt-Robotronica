1af028128a778d126d331096e32034d104d2c474
Poroto <poro.ferreira@gmail.com>
2020-09-27 17:17:12 -0300

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -16,6 +16,15 @@ enum MyEnum {
  */
 //% weight=100 color=#0fbc11 icon=""
 namespace Robotrónica {
+      /**
+       * Retorna el número de Fibonacci de la posición indicada.
+       * @param La posición del número de Fibonacci a mostrar, eg: 6
+       */
+      //% block
+      export function fibonacci(value: number): number {
+          return value <= 1 ? value : fibonacci(value -1) + fibonacci(value - 2);
+      }
+
     /**
      * Muestra una cadena de texto con la dirección de Robotrónica.
      */
@@ -31,24 +40,4 @@ namespace Robotrónica {
     export function decirRobotrónica(): void {
         basic.showString("Robotrónica");
     }
-
-    /**
-     * Retorna el número de Fibonacci de la posición indicada.
-     * @param La posición del número de Fibonacci a mostrar, eg: 6
-     */
-    //% block
-    export function fibonacci(value: number): number {
-        return value <= 1 ? value : fibonacci(value -1) + fibonacci(value - 2);
-    }
-
-    /**
-     * TODO: describa su función aquí
-     * @param n describa el parámetro aquí, eg: 5
-     * @param s describa el parámetro aquí, eg: "Hello"
-     * @param e describa el parámetro aquí
-     */
-    //% block
-    /*export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }*/
 }
