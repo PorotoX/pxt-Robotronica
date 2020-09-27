6202e33e7c2536e94aab5a707f8e60d31eb123a5
Poroto <poro.ferreira@gmail.com>
2020-09-27 17:14:09 -0300

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -17,6 +17,31 @@ enum MyEnum {
 //% weight=100 color=#0fbc11 icon=""
 namespace Robotrónica {
     /**
+     * Muestra una cadena de texto con la dirección de Robotrónica.
+     */
+    //% block
+    export function direcciónRobotrónica(): string {
+        return 'Brasil 1086';
+    }
+
+    /**
+     * Muestra la cadena de texto "Robotrónica".
+     */
+    //% block
+    export function decirRobotrónica(): void {
+        basic.showString("Robotrónica");
+    }
+
+    /**
+     * Retorna el número de Fibonacci de la posición indicada.
+     * @param La posición del número de Fibonacci a mostrar, eg: 6
+     */
+    //% block
+    export function fibonacci(value: number): number {
+        return value <= 1 ? value : fibonacci(value -1) + fibonacci(value - 2);
+    }
+
+    /**
      * TODO: describa su función aquí
      * @param n describa el parámetro aquí, eg: 5
      * @param s describa el parámetro aquí, eg: "Hello"
@@ -26,30 +51,4 @@ namespace Robotrónica {
     /*export function foo(n: number, s: string, e: MyEnum): void {
         // Add code here
     }*/
-
-    /**
-     * Muestra una cadena de texto con la dirección de Robotrónica.
-     */
-    //% block
-    export function direcciónRobotrónica(): string {
-        return 'Brasil 1086';
-    }
-    
-    /**
-     * Muestra la cadena de texto "Robotrónica".
-     */
-
-    //% block
-    export function decirRobotrónica(): void {
-        basic.showString("Robotrónica");
-    }
-
-    /**
-     * Retorna el número de Fibonacci del número indicado.
-     * @param La posición del número de Fibonacci a mostrar, eg: 6
-     */
-    //% block
-    export function fibonacci(value: number): number {
-        return value <= 1 ? value : fibonacci(value -1) + fibonacci(value - 2);
-    }
 }
